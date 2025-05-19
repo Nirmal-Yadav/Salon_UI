9d95430f55c43019d7a3031f1312b8d52307a1c7
Nirmal Yadav <yadavnirmal07@gmail.com>
2025-05-19 16:20:41 +0530

    main js updated

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -7,12 +7,28 @@ function Main() {
     <div className="main-container-1">
       <div className="header">
         <div className="t1">
-          The Barber Shop Come <br /> And Experience
+          THE BARBER SHOP <br />
+          Do Come And Experience
         </div>
-        {text.split("").map((char, i) => (
-          <span key={i} style={{ animationDelay: `${i * 0.3}s` }}>
-            {char === " " ? "\u00A0" : char}
-          </span>
+        {text.split(" ").map((word, wordIndex) => (
+          <React.Fragment key={wordIndex}>
+            {word === "Talking" && <br />}
+            {word.split("").map((char, i) => (
+              <span
+                key={`${wordIndex}-${i}`}
+                className="t2"
+                style={{ animationDelay: `${(wordIndex * 6 + i) * 0.1}s` }}
+              >
+                {char}
+              </span>
+            ))}
+            <span
+              className="t2"
+              style={{ animationDelay: `${(wordIndex + 1) * 0.6}s` }}
+            >
+              {"\u00A0"}
+            </span>
+          </React.Fragment>
         ))}
       </div>
       <button className="review-button">Our Review</button>
