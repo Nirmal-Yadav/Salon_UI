39c073123e43eb08160e958eccb232e3529b5f4b
Nirmal Yadav <yadavnirmal07@gmail.com>
2025-05-19 16:20:32 +0530

    header updated

--- src/pages/Header.jsx
+++ src/pages/Header.jsx
@@ -1,7 +1,27 @@
 import React from "react";
+import "../css/Header.css";
+import img from "../assets/salon-logo.jpg";
 
 function Header() {
-  return <div className="header-container"></div>;
+  return (
+    <div className="header-container">
+      <div className="header-content">
+        <div>
+          <img src={img} className="salon-logo" />
+        </div>
+        <div className="head-option-container">
+          <span>MENU</span>
+          <span>PRICING</span>
+          <span>MEMBERSHIP</span>
+          <span>ABOUT US</span>
+          <span>GALLERY</span>
+        </div>
+        <div>
+          <button className="appointment-button">APPOINTMENT</button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
