08c543aade164f9aaa63edf68a492a392d8ad9b0
Nirmal Yadav <yadavnirmal07@gmail.com>
2025-05-20 12:07:52 +0530

    dashboard and main update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./Header";
 import Main from "./main";
+import Section1 from "./Section1";
 
 function Dashboard() {
   return (
     <div className="dashboard-container">
       <Header />
       <Main />
+      <Section1 />
     </div>
   );
 }
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,38 +1,41 @@
 import React from "react";
 import "../css/Header.css";
+import Section1 from "./Section1";
 
 function Main() {
   const text = "Let Your Hair Do The Talking";
   return (
-    <div className="main-container-1">
-      <div className="header">
-        <div className="t1">
-          THE BARBER SHOP <br />
-          Do Come And Experience
-        </div>
-        {text.split(" ").map((word, wordIndex) => (
-          <React.Fragment key={wordIndex}>
-            {word === "Talking" && <br />}
-            {word.split("").map((char, i) => (
+    <>
+      <div className="main-container-1">
+        <div className="header">
+          <div className="t1">
+            THE BARBER SHOP <br />
+            Do Come And Experience
+          </div>
+          {text.split(" ").map((word, wordIndex) => (
+            <React.Fragment key={wordIndex}>
+              {word === "Talking" && <br />}
+              {word.split("").map((char, i) => (
+                <span
+                  key={`${wordIndex}-${i}`}
+                  className="t2"
+                  style={{ animationDelay: `${(wordIndex * 6 + i) * 0.1}s` }}
+                >
+                  {char}
+                </span>
+              ))}
               <span
-                key={`${wordIndex}-${i}`}
                 className="t2"
-                style={{ animationDelay: `${(wordIndex * 6 + i) * 0.1}s` }}
+                style={{ animationDelay: `${(wordIndex + 1) * 0.6}s` }}
               >
-                {char}
+                {"\u00A0"}
               </span>
-            ))}
-            <span
-              className="t2"
-              style={{ animationDelay: `${(wordIndex + 1) * 0.6}s` }}
-            >
-              {"\u00A0"}
-            </span>
-          </React.Fragment>
-        ))}
+            </React.Fragment>
+          ))}
+        </div>
+        <button className="review-button">Our Review</button>
       </div>
-      <button className="review-button">Our Review</button>
-    </div>
+    </>
   );
 }
 
