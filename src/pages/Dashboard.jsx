713dd7ff9597bd9c402912436c864c3394388227
Nirmal Yadav <yadavnirmal07@gmail.com>
2025-05-21 16:18:07 +0530

    sec4

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -4,6 +4,7 @@ import Main from "./Main.jsx";
 import Section1 from "./Section1.jsx";
 import Section2 from "./Section2.jsx";
 import Section3 from "./Section3.jsx";
+import Section4 from "./Section4.jsx";
 
 function Dashboard() {
   return (
@@ -13,6 +14,7 @@ function Dashboard() {
       <Section1 />
       <Section2 />
       <Section3 />
+      <Section4 />
     </div>
   );
 }
---
+++ src/pages/Section4.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import img1 from "../assets/hair-sec4-img1.jpg";
+import img2 from "../assets/hair-sec4-img2.jpg";
+import img3 from "../assets/hair-sec4-img3.jpg";
+import img4 from "../assets/hair-sec4-img4.jpg";
+import img5 from "../assets/hair-sec4-img5.jpg";
+
+function Section4() {
+  return (
+    <div className="sec4-container">
+      <div className="sec4-t1">Our Works</div>
+      <div className="sec4-t2">Barbers are Magicians without a Wand...</div>
+      <div className="sec4-img-container">
+        <div className="sec4-img-con">
+          <img src={img1} className="sec4-img1" />
+          <div className="sec4-t-con">
+            <div className="sec4-img-t1"> Haircut, Hairstyle</div>
+            <div className="sec4-img-t2">Haircut & Blowdry</div>
+          </div>
+        </div>
+
+        <div className="sec4-img-con">
+          <img src={img2} className="sec4-img2" />
+          <div className="sec4-t-con">
+            <div className="sec4-img-t1"> Haircut, Hairstyle</div>
+            <div className="sec4-img-t2">Haircut & Blowdry</div>
+          </div>
+        </div>
+
+        <div className="sec4-img-con">
+          <img src={img3} className="sec4-img3" />
+          <div className="sec4-t-con">
+            <div className="sec4-img-t1"> Haircut, Hairstyle</div>
+            <div className="sec4-img-t2">Haircut & Blowdry</div>
+          </div>
+        </div>
+
+        <div className="sec4-img-con">
+          <img src={img4} className="sec4-img4" />
+          <div className="sec4-t-con">
+            <div className="sec4-img-t1"> Haircut, Hairstyle</div>
+            <div className="sec4-img-t2">Haircut & Blowdry</div>
+          </div>
+        </div>
+
+        <div className="sec4-img-con">
+          <img src={img5} className="sec4-img5" />
+          <div className="sec4-t-con">
+            <div className="sec4-img-t1"> Haircut, Hairstyle</div>
+            <div className="sec4-img-t2">Haircut & Blowdry</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Section4;
