7a58fce0a1a97b3f0c5d01f84a6443cd3d35654c
Nirmal Yadav <yadavnirmal07@gmail.com>
2025-05-21 12:44:25 +0530

    section2

---
+++ src/pages/Section2.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import Card from "../components/Card";
+
+function Section2() {
+  return (
+    <div className="section2-container">
+      <div className="section2-t1">Our Services</div>
+      <div className="section2-t2">
+        At HMX By Hairmechanixx our mission is to ensure that everyone who
+        visits us
+        <br />
+        {`\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0
+          \u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0`}
+        walks away feeling and looking their best.
+      </div>
+      <div className="section2-card-container">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    </div>
+  );
+}
+
+export default Section2;
