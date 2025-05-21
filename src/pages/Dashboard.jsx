fe6f5449183d24c0afa749b28ad47b4990bcaf81
Nirmal Yadav <yadavnirmal07@gmail.com>
2025-05-21 16:44:02 +0530

    section 5

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -5,6 +5,7 @@ import Section1 from "./Section1.jsx";
 import Section2 from "./Section2.jsx";
 import Section3 from "./Section3.jsx";
 import Section4 from "./Section4.jsx";
+import Section5 from "./Section5.jsx";
 
 function Dashboard() {
   return (
@@ -15,6 +16,7 @@ function Dashboard() {
       <Section2 />
       <Section3 />
       <Section4 />
+      <Section5 />
     </div>
   );
 }
---
+++ src/pages/Section5.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Section5() {
+  return (
+    <div className="sec5-container">
+      <div className="sec5-t1">Working Hours</div>
+      <div className="sec5-t2">
+        Great Hair never happens by Chance It happens by Appointments
+      </div>
+      <div className="sec5-t3">
+        <span className="sec5-day">Weekdays</span>
+        <span className="sec5-time">11AM - 10PM</span>
+      </div>
+      <div className="sec5-t4">
+        <span className="sec5-day">Weekend</span>
+        <span className="sec5-time">10AM - 11AM</span>
+      </div>
+    </div>
+  );
+}
+
+export default Section5;
