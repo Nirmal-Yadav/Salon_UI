2694990ed1ef020e53062fe4f988e47df3ccf856
Nirmal Yadav <yadavnirmal07@gmail.com>
2025-05-21 12:44:39 +0530

    dashboard update

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Main from "./main";
 import Section1 from "./Section1";
+import Section2 from "./Section2";
 
 function Dashboard() {
   return (
@@ -9,6 +10,7 @@ function Dashboard() {
       <Header />
       <Main />
       <Section1 />
+      <Section2 />
     </div>
   );
 }
