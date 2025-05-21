caa8b88bafaaa82a474b2d8cb8862cdd635f856a
Nirmal Yadav <yadavnirmal07@gmail.com>
2025-05-21 12:44:16 +0530

    card js

---
+++ src/components/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="card-container">
+      <div className="card-t1">Beard Color</div>
+      <div className="card-t2">
+        Consultation Color application Wait for 20-25 min (depends on the shade)
+        Wash with beard shampoo & conditioner Beard dry & styling.
+      </div>
+      <button className="card-button">Read More</button>
+    </div>
+  );
+}
+
+export default Card;
