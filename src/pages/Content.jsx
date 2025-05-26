7f85b0d85888fdbf6a514a869c5816eb0a266002
Nirmal Yadav <yadavnirmal07@gmail.com>
2025-05-26 12:36:35 +0530

    updating content file

---
+++ src/pages/Content.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import Main from "./Main.jsx";
+import Section1 from "./Section1.jsx";
+import Section2 from "./Section2.jsx";
+import Section3 from "./Section3.jsx";
+import Section4 from "./Section4.jsx";
+import Section5 from "./Section5.jsx";
+
+function Content() {
+  return (
+    <div>
+      <Main />
+      <Section1 />
+      <Section2 />
+      <Section3 />
+      <Section4 />
+      <Section5 />
+    </div>
+  );
+}
+
+export default Content;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,24 +1,15 @@
 import React from "react";
 import Header from "./Header.jsx";
-import Main from "./Main.jsx";
-import Section1 from "./Section1.jsx";
-import Section2 from "./Section2.jsx";
-import Section3 from "./Section3.jsx";
-import Section4 from "./Section4.jsx";
-import Section5 from "./Section5.jsx";
+
 import Footer from "./Footer.jsx";
 import "../css/Dashboard.css";
+import Content from "./Content.jsx";
 
 function Dashboard() {
   return (
     <div className="dashboard-container">
       <Header />
-      <Main />
-      <Section1 />
-      <Section2 />
-      <Section3 />
-      <Section4 />
-      <Section5 />
+      <Content />
       <Footer />
     </div>
   );
